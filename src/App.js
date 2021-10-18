9c9f18f02ef7061933678e74a1a608c22d89055d
Tomasz Lewandowski <tomek2x1@wp.pl>
2021-10-18 03:17:45 +0200

    uwagi Łukasza

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import Input from "./Components/Input";
+import InputRadio from "./Components/InputRadio";
 import Select from "./Components/Select";
 import TextArea from "./Components/TextArea";
 import InputFile from "./Components/InputFile";
@@ -28,6 +29,10 @@ const App = () => {
     street: "",
     zipCode: "",
     city: "",
+    deviceSamePlace: "",
+    street2: "",
+    zipCode2: "",
+    city2: "",
     getBack: "",
     agreement: "",
   });
@@ -73,6 +78,10 @@ const App = () => {
     zipCode: "",
     city: "",
     getBack: "",
+    deviceSamePlace: "",
+    street2: "",
+    zipCode2: "",
+    city2: "",
     agreement: "",
   });
 
@@ -105,6 +114,10 @@ const App = () => {
       zipCode: false,
       city: false,
       getBack: false,
+      deviceSamePlace: false,
+      street2: false,
+      zipCode2: false,
+      city2: false,
       agreement: false,
     };
 
@@ -161,6 +174,25 @@ const App = () => {
 
     if (state.getBack === "") validate.getBack = true;
 
+    if (
+      (state.typeOfReturn === "Gwarancja producenta" ||
+        state.typeOfReturn === "Rękojmia") &&
+      state.requiresDisassembly === "Tak"
+    ) {
+      if (state.deviceSamePlace === "") validate.deviceSamePlace = true;
+    }
+
+    if (
+      (state.typeOfReturn === "Gwarancja producenta" ||
+        state.typeOfReturn === "Rękojmia") &&
+      state.requiresDisassembly === "Tak" &&
+      state.deviceSamePlace === "Nie"
+    ) {
+      if (state.street2 === "") validate.street2 = true;
+      if (state.zipCode2 === "") validate.zipCode2 = true;
+      if (state.city2 === "") validate.city2 = true;
+    }
+
     if (state.agreement === "") validate.agreement = true;
 
     setBadValidate({ ...badValidate, ...validate });
@@ -182,20 +214,23 @@ const App = () => {
       !validate.getBack &&
       !validate.agreement
     ) {
-      if (state.typeOfReturn === "Gwarancja producenta") {
-        if (!validate.buyDate && !validate.requiresDisassembly) {
-          sendForm(state);
-        }
-      } else if (
+      if (
         state.typeOfReturn === "Gwarancja producenta" ||
-        state.typeOfReturn === "Rękojmia"
+        (state.typeOfReturn === "Rękojmia" && state.howFinish)
       ) {
         if (
-          !validate.buyDate &&
-          !validate.howFinish &&
-          !validate.requiresDisassembly
+          !validate.requiresDisassembly &&
+          state.requiresDisassembly === "Nie"
         ) {
           sendForm(state);
+        } else if (state.requiresDisassembly === "Tak") {
+          if (state.deviceSamePlace === "Tak") {
+            sendForm(state);
+          } else if (state.deviceSamePlace === "Nie") {
+            if (state.street2 && state.zipCode2 && state.city2) {
+              sendForm(state);
+            }
+          }
         }
       } else if (
         state.typeOfReturn ===
@@ -220,25 +255,26 @@ const App = () => {
     "Obniżenie ceny towaru",
     "Odstąpienie od umowy (istotna wada towaru)",
   ];
-
   const isTrue = ["Tak", "Nie"];
 
+  const deviceSamePlace = ["Tak", "Nie"];
+
   const producer = [
-    "Blaupunkt", 
-    "Climative", 
-    "Danfoss", 
-    "Devi", 
-    "Digitime", 
-    "Dimplex", 
-    "Ebeco", 
-    "Eberle", 
-    "Emko", 
-    "Esco", 
-    "Nexans", 
-    "Rotenso", 
-    "Sonniger", 
-    "Thermoval", 
-    "Vaco", 
+    "Blaupunkt",
+    "Climative",
+    "Danfoss",
+    "Devi",
+    "Digitime",
+    "Dimplex",
+    "Ebeco",
+    "Eberle",
+    "Emko",
+    "Esco",
+    "Nexans",
+    "Rotenso",
+    "Sonniger",
+    "Thermoval",
+    "Vaco",
     "Warmtec",
     "Inny",
   ];
@@ -304,6 +340,25 @@ const App = () => {
       <b>Ulica:</b> ${obj.street}<br/>
       <b>Kod pocztowy:</b> ${obj.zipCode} <br/>
       <b>Miejscowość:</b> ${obj.city} <br/>
+      ${
+        obj.typeOfReturn === "Gwarancja producenta" ||
+        obj.typeOfReturn === "Rękojmia"
+          ? `<b>Adres urządzenia taki sam jak adres powyżej:</b> ${obj.deviceSamePlace} <br/>`
+          : ""
+      }
+
+      ${
+        (obj.typeOfReturn === "Gwarancja producenta" ||
+          obj.typeOfReturn === "Rękojmia") &&
+        obj.requiresDisassembly === "Tak" &&
+        obj.deviceSamePlace === "Nie"
+          ? `<b>Adres urządzenia: 
+          Ulica: </b> ${obj.street2} <br/>
+          <b>Kod pocztowy: </b> ${obj.zipcode} <br/>
+          <b>Miejscowość: </b> ${obj.zipcode} <br/>
+          `
+          : ""
+      }
       <br/>
       <b>Preferowana data odbioru:</b> ${obj.getBack} <br/>
       `,
@@ -344,7 +399,6 @@ const App = () => {
       {showForm ? (
         <div id="return-form" className="return-form">
           <h2 className="return-form__title">Formularz reklamacji (online)</h2>
-
           <Input
             value={state.docNumber}
             name={"docNumber"}
@@ -354,7 +408,6 @@ const App = () => {
             validation={badValidate.docNumber}
             errorMsg={"Podaj numer zamówienia lub numer faktury"}
           />
-
           <Select
             value={state.producer}
             optionsValue={producer}
@@ -364,7 +417,6 @@ const App = () => {
             validation={badValidate.producer}
             errorMsg={"Podaj nazwę producenta"}
           />
-
           <Input
             value={state.typeProduct}
             name={"typeProduct"}
@@ -374,7 +426,6 @@ const App = () => {
             validation={badValidate.typeProduct}
             errorMsg={"Podaj nazwę i model towaru"}
           />
-
           <Input
             value={state.quantity}
             name={"quantity"}
@@ -385,7 +436,16 @@ const App = () => {
             validation={badValidate.quantity}
             errorMsg={"Podaj ilość"}
           />
-
+          <Input
+            value={state.buyDate}
+            name={"buyDate"}
+            labelName="Data zakupu"
+            handleInput={handleInput}
+            type={"date"}
+            validation={badValidate.buyDate}
+            maxDate={todayDate}
+            errorMsg={"Podaj datę zakupu"}
+          />
           <Select
             value={state.typeOfReturn}
             name={"typeOfReturn"}
@@ -395,16 +455,6 @@ const App = () => {
             validation={badValidate.typeOfReturn}
             errorMsg={"Podaj rodzaj zgłoszenia"}
           />
-            <Input
-              value={state.buyDate}
-              name={"buyDate"}
-              labelName="Data zakupu"
-              handleInput={handleInput}
-              type={"date"}
-              validation={badValidate.buyDate}
-              maxDate={todayDate}
-              errorMsg={"Podaj datę zakupu"}
-            />
           {state.typeOfReturn === "Rękojmia" ? (
             <Select
               value={state.howFinish}
@@ -416,7 +466,6 @@ const App = () => {
               errorMsg={"Podaj sposób zakończenia zgłoszenia"}
             />
           ) : null}
-
           {state.typeOfReturn ===
           "Dostałem towar uszkodzony (zgł. max. do 7 dni od otrzymania towaru)" ? (
             <Select
@@ -441,7 +490,6 @@ const App = () => {
               errorMsg={"Czy produkt wymaga demontażu"}
             />
           ) : null}
-
           <TextArea
             value={state.description}
             name={"description"}
@@ -450,7 +498,6 @@ const App = () => {
             validation={badValidate.description}
             errorMsg={"Podaj opis wady"}
           />
-
           <InputFile
             value={state.files}
             name={"files"}
@@ -459,7 +506,6 @@ const App = () => {
             handleInput={handleInput}
             validation={badValidate.files}
           />
-
           <Input
             value={state.name}
             name={"name"}
@@ -469,7 +515,6 @@ const App = () => {
             validation={badValidate.name}
             errorMsg={"Podaj imię i nazwisko lub nazwa firmy"}
           />
-
           <Input
             value={state.email}
             name={"email"}
@@ -479,7 +524,6 @@ const App = () => {
             validation={badValidate.email}
             errorMsg={"Podaj adres e-mail"}
           />
-
           <Input
             value={state.phone}
             name={"phone"}
@@ -489,7 +533,6 @@ const App = () => {
             validation={badValidate.phone}
             errorMsg={"Podaj telefon kontaktowy"}
           />
-
           <Input
             value={state.street}
             name={"street"}
@@ -499,7 +542,6 @@ const App = () => {
             validation={badValidate.street}
             errorMsg={"Podaj ulicę i numer domu"}
           />
-
           <Input
             value={state.zipCode}
             name={"zipCode"}
@@ -509,7 +551,6 @@ const App = () => {
             validation={badValidate.zipCode}
             errorMsg={"Podaj kod pocztowy"}
           />
-
           <Input
             value={state.city}
             name={"city"}
@@ -519,7 +560,56 @@ const App = () => {
             validation={badValidate.city}
             errorMsg={"Podaj miejscowość"}
           />
+          {(state.typeOfReturn === "Gwarancja producenta" ||
+            state.typeOfReturn === "Rękojmia") &&
+          state.requiresDisassembly === "Tak" ? (
+            <InputRadio
+              value={state.deviceSamePlace}
+              name={"deviceSamePlace"}
+              labelName={`Adres urządzenia taki sam jak powyżej?`}
+              handleInput={handleInput}
+              validation={badValidate.deviceSamePlace}
+              options={deviceSamePlace}
+              errorMsg={`Podaj adres urządzenia`}
+            />
+          ) : null}
+          {(state.typeOfReturn === "Gwarancja producenta" ||
+            state.typeOfReturn === "Rękojmia") &&
+          state.requiresDisassembly === "Tak" &&
+          state.deviceSamePlace === "Nie" ? (
+            <div>
+              <div className="return-form__subtitle">Dane urządzenia:</div>
+              <Input
+                value={state.street2}
+                name={"street2"}
+                labelName="Ulica i numer domu"
+                handleInput={handleInput}
+                type={"text"}
+                validation={badValidate.street2}
+                errorMsg={"Podaj ulicę i numer domu"}
+              />
 
+              <Input
+                value={state.zipCode2}
+                name={"zipCode2"}
+                labelName="Kod pocztowy"
+                handleInput={handleInput}
+                type={"number"}
+                validation={badValidate.zipCode2}
+                errorMsg={"Podaj kod pocztowy"}
+              />
+
+              <Input
+                value={state.city2}
+                name={"city2"}
+                labelName="Miejscowość"
+                handleInput={handleInput}
+                type={"text"}
+                validation={badValidate.city2}
+                errorMsg={"Podaj miejscowość"}
+              />
+            </div>
+          ) : null}
           <Input
             value={state.getBack}
             name={"getBack"}
@@ -530,14 +620,12 @@ const App = () => {
             minValue={tomorrowDate}
             errorMsg={"Podaj preferowaną datę odbioru"}
           />
-
           <Agreement
             value={state.agreement}
             name={"agreement"}
             handleCheckbox={handleCheckbox}
             validation={badValidate.agreement}
           />
-
           <div className="return-form__btn-wrapper">
             <button
               className="return-form__btn"
@@ -578,8 +666,7 @@ const App = () => {
               Formularz został wysłany poprawnie
             </h2>
             <h3 className="return-message__id">
-              Numer Twojego zgłoszenia:
-              <strong>{taskNumber}</strong>
+              Numer Twojego zgłoszenia: <strong>{taskNumber}</strong>
             </h3>
             <p className="return-message__remember">
               * Potwierdzenie wysłania formularza znajdziesz na swojej skrzynce
